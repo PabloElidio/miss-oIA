f637506fc1b074e1245ca94bfbdc2560e9fbabd8
PabloElidio <145361911+PabloElidio@users.noreply.github.com>
2025-07-01 11:39:05 -0300

    daw

---
+++ script.js
@@ -0,0 +1,37 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const textoResultado = document.querySelector(".texto3-resultado");
+
+const perguntas =[
+    {
+        enuciado:"Assim que saiu da escola ele viu algo",
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso"
+        ]
+    },    
+    {
+        enuciado:"Assim que saiu da escola ele viu algo",
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso"
+        ]
+    },
+    {
+
+        enuciado:"Assim que saiu da escola ele viu algo",
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso"
+        ]
+    },
+    {
+
+        enuciado:"Assim que saiu da escola ele viu algo",
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso"
+        ]
+    }
+]
